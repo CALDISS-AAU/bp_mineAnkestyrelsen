8f6b8c609c9e02625660a67eb871d170a88575d4
Kristian Kjelmann <kgk@adm.aau.dk>
2022-11-30 15:44:32 +0100

    init repo

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+pip install pdfminer-six
+pip install pandas
+pip install nltk
+pip install spacy
+pip install PyPDF2
+pip install openpyxl
+
+python -m spacy download da_core_news_lg
