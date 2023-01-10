2ff48ea66ccf17d362d5a5213f4639afa1bb0c01
Kristian Gade Kjelmann <kgk@adm.aau.dk>
2023-01-10 12:09:21 +0100

    update req

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-sudo apt-get install poppler-utils
-sudo apt-get install tesseract-ocr
+sudo apt-get install -y poppler-utils
+sudo apt-get install -y tesseract-ocr
 
 sudo wget https://github.com/tesseract-ocr/tessdata/raw/4.00/dan.traineddata -P /usr/share/tesseract-ocr/4.00/tessdata
 
